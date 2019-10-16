2c9ac5eac4796c83ca23d9a3a084d467ded26ddf
MPThLee <mpthlee@gmail.com>
2019-10-16 19:20:23 +0900

    Squiz documentize is done with No file comment

--- build/template.php
+++ build/template.php
@@ -3,13 +3,13 @@ require_once './vendor/autoload.php';
 
 use LightnCandy\LightnCandy;
 
-$phpFilePrefix  = "\nnamespace RSSMerger\\template;\n";
-$template_dir   = __DIR__.'/../src/template/';
-$template_files = preg_grep('~\.(handlebars|mustache)$~', scandir($template_dir));
+$phpFilePrefix = "\nnamespace RSSMerger\\template;\n";
+$templateDir   = __DIR__.'/../src/template/';
+$templateFiles = preg_grep('~\.(handlebars|mustache)$~', scandir($templateDir));
 
-foreach ($template_files as $file) {
-    $template = file_get_contents($template_dir.$file);
+foreach ($templateFiles as $file) {
+    $template = file_get_contents($templateDir.$file);
     $phpStr   = LightnCandy::compile($template);
     $data     = '<?php '.$phpFilePrefix.$phpStr.'?>';
-    file_put_contents($template_dir.strtok($file, '.').'.php', $data);
+    file_put_contents($templateDir.strtok($file, '.').'.php', $data);
 }
